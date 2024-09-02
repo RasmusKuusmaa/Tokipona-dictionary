626c2e8506ed7583fe321fcc65bf273db6f50851
77f315 <77f315@gmail.com>
2024-09-02 17:48:49 +0300

    looks like i missed a couple of words

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import json
 
-with open('dictionary.json', 'r') as file:
+with open('./dictionary.json', 'r') as file:
     data = json.load(file)
 while True:
     word = input('enter word:')
     if word in data:
         print(word,' def:' ,data[word]['meaning'])
     else:
+        break
         print('word not in dictionary')
